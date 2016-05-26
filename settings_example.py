a18ff5cf795c01fe9324572da13c5b0eb18b082e
Avi Solomon <genepool99@gmail.com>
2016-05-26 16:27:33 -0700

    add example settings file

---
+++ settings_example.py
@@ -0,0 +1,9 @@
+# This is the secerets file, keep untracked by source control
+# rename settings.py and save in script root directory
+SF_USERNAME = "example@domain.com"                          # website login
+SF_PASSWORD = "mylogin"                                     # website password
+SF_DEBUG = False                                            # debugging on/off default = False
+SF_PKEY = "your production api key"                         # PRODUCTION API Key
+SF_PCKEY = "your production clinic key"                     # PRODUCTION Clinic API Key
+SF_SKEY = "your sandbox api key"                            # SANDBOX API Key
+SF_SCKEY = "your sandbox clinic key"                        # SANDBOX Clinic API Key
