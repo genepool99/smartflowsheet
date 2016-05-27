765e59fc4d7c9fcb47627a70ff04ff299f86a4fe
Avi Solomon <asolomon@dovelewis.org>
2016-05-26 20:39:35 -0400

    expand settings examples and new hospitalization functions

--- settings_example.py
+++ settings_example.py
@@ -1,9 +1,11 @@
 # This is the secerets file, keep untracked by source control
 # rename settings.py and save in script root directory
 SF_USERNAME = "example@domain.com"                          # website login
-SF_PASSWORD = "mylogin"                                     # website password
+SF_PASSWORD = "mypassword"                                  # website password
 SF_DEBUG = False                                            # debugging on/off default = False
 SF_PKEY = "your production api key"                         # PRODUCTION API Key
 SF_PCKEY = "your production clinic key"                     # PRODUCTION Clinic API Key
 SF_SKEY = "your sandbox api key"                            # SANDBOX API Key
 SF_SCKEY = "your sandbox clinic key"                        # SANDBOX Clinic API Key
+SF_USE_SANDBOX = True                                       # Switch between sandbox and production
+SF_HOOK_HANDLER = 'yourHandlerURL'                          # the url to setup a webhook handler
--- sfs.py
+++ sfs.py
@@ -16,16 +16,20 @@ class sfs:
     """Common base class for all smartflow sessions"""
     
     # This is the Production URL
-    #baseURL = "https://www.smartflowsheet.com/api/v3"
+    baseProductionURL = "https://www.smartflowsheet.com/api/v3"
     
     # This is the Sandbox URL (uncomment to use)
-    baseURL = "https://sfs-public.azurewebsites.net/api/v3"
+    baseSandboxURL = "https://sfs-public.azurewebsites.net/api/v3"
 
-    def __init__(self, key, ckey, debug = False):
+    def __init__(self, key, ckey, debug, sandbox):
         self.key = key
         self.ckey = ckey
         self.debug = debug
         self.s = requests.session()
+        if sandbox:
+            self.baseURL = self.baseSandboxURL
+        else:
+            self.baseURL = self.baseProductionURL
         
     def register_hook(self, url):
         ''' Register a custom webhook handler URL
@@ -34,7 +38,7 @@ class sfs:
             Returns: request
         '''
         headers = {'User-Agent':'MyClient/1.0.0', 'Content-type':'application/json',  'emrApiKey':self.key, 'clinicApiKey':self.ckey}
-        url = sfs.baseURL + '/account/webhook?url=' + str(url)
+        url = self.baseURL + '/account/webhook?url=' + str(url)
         try:
             r = self.s.post(url, headers=headers)
         except requests.exceptions.ConnectionError as e:
@@ -43,10 +47,33 @@ class sfs:
         return r
         
     def get_departments(self):
-        ''' Return a list of hospital departments '''
+        ''' Return a dictionary of hospital departments '''
         headers = {'User-Agent':'MyClient/1.0.0', 'Content-type':'application/json',  'emrApiKey':self.key, 'clinicApiKey':self.ckey}
         try:
-            r = self.s.get(sfs.baseURL + '/departments', headers=headers, timeout=5)
+            r = self.s.get(self.baseURL + '/departments', headers=headers, timeout=5)
+            data = json.loads(r.text)
+        except:
+            return None
+        return data
+        
+    def get_active_hosp(self):
+        ''' Return a dictionary of active hospiltaizations '''
+        headers = {'User-Agent':'MyClient/1.0.0', 'Content-type':'application/json',  'emrApiKey':self.key, 'clinicApiKey':self.ckey}
+        try:
+            r = self.s.get(self.baseURL + '/hospitalizations', headers=headers, timeout=5)
+            data = json.loads(r.text)
+        except:
+            return None
+        return data
+        
+    def get_hosp(self, hid):
+        ''' Return a hospiltaization object or None'''
+        headers = {'User-Agent':'MyClient/1.0.0', 'Content-type':'application/json',  'emrApiKey':self.key, 'clinicApiKey':self.ckey}
+        try:
+            url = self.baseURL + '/hospitalization/' + str(hid)
+            print url
+            r = self.s.get(url, headers=headers, timeout=5)
+            print r.status_code
             data = json.loads(r.text)
         except:
             return None
